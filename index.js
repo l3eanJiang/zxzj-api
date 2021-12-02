d16e61640e803f3cd6b45fec3d31406917b49054
ComeEatMeat <1163573416@qq.com>
2021-12-02 17:16:10 +0800

    1

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const axios = require('axios').default
+const cheerio = require('cheerio')
+const baseUrl = 'https://www.zxzj.fun'
 
-axios.get('https://www.zxzj.fun/list/1.html').then((res) => {
-  console.log(res)
+axios.get(baseUrl + '/list/1.html').then((res) => {
+  const $ = cheerio.load(res.data)
+  const result = []
+  $('.stui-vodlist__thumb').each(() => {
+    const item = $(this)
+    let title = item.prop('title')
+    let href = baseUrl + item.prop('href')
+    let imgUrl = item.data('original')
+    let status = item.children('pic-text').text()
+    result.push({title, href, imgUrl, status})
+  })
+  console.log(result)
 })
